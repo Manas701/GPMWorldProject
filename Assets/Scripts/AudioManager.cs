d8c2fcffe42b209011ac7d857ac1577180b8bae1
david <987582430@qq.com>
2023-12-06 21:01:00 -0800

    add text beep sound

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,15 +7,19 @@ public class AudioManager : MonoBehaviour
     [SerializeField]
     private AudioSource popUpAudioSource;
     [SerializeField]
+    private AudioSource textAudioSource;
+    [SerializeField]
     private AudioClip popUpSfx;
-
+    [SerializeField]
+    private AudioClip textSfx;
+    private int a = 0;
     // Start is called before the first frame update
     void Start()
     {
         //playaudio on new text
         GameManager.current.onSetDialogue += PlayPopSound;
         //playaudio one by one word
-        //GameManager.current.onDialoguePlayText += PlayPopSound;
+        GameManager.current.onDialoguePlayText += TextSound;
     }
 
     // Update is called once per frame
@@ -31,9 +35,14 @@ public class AudioManager : MonoBehaviour
         popUpAudioSource.Play();
     }
     void TextSound()
-    { 
-    
-    
+
+    {
+        if (!textAudioSource.isPlaying)
+        {
+            textAudioSource.clip = textSfx;
+            textAudioSource.Play();
+        }
+       
     }
 
 }
