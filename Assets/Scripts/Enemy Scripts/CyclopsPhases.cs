013fa13225a20f0f19bca221e048f2706c988a8b
Lowliet <lowdata114@gmail.com>
2023-12-07 22:59:45 -0800

    Cyclops Boss Implemented
    Boss Animations
    Phases work

--- Assets/Scripts/Enemy Scripts/CyclopsPhases.cs
+++ Assets/Scripts/Enemy Scripts/CyclopsPhases.cs
@@ -2,35 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum phases { phase1, phase2, phase3}
+public enum phases { phase1, phase2, phase3 }
 
 public class CyclopsPhases : MonoBehaviour
 {
 
     public phases currentPhase;
     public int currentHealth;
+    public Animator bossAnimator;
+    public float actionCooldownTime;
+    private bool canAction;
+    private string[] phaseOneTriggerNames = { "PlayRightHandSlam", "PlayLeftHandSlam", "PlayDiagnolSlam", "PlayDiagnolSlam", "PlayDiagnolSlam" };
+    private string[] phaseTwoTriggerNames = { "PlayRightHandSlam", "PlayLeftHandSlam", "PlayDiagnolSlam", "PlayHeadShootLaser" };
+    private string[] phaseThreeTriggerNames = { "PlayHeadShootLaser", "PlayHorizontalLaser", "PlayVerticalLaser" };
 
 
     // Start is called before the first frame update
     void Start()
     {
         currentPhase = phases.phase1;
+        canAction = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        updatePhase();
-        actOnPhases(currentPhase);
+        if (canAction == true)
+        {
+            StartCoroutine(initiateActionCooldown());
+            updatePhase();
+            actOnPhases(currentPhase);
+        }
     }
 
     void updatePhase()
     {
-       if(currentHealth < 75 && currentHealth >50)
+        if (currentHealth < 75 && currentHealth > 50)
         {
             currentPhase = phases.phase2;
         }
-       else if( currentHealth < 50 && currentHealth > 0)
+        else if (currentHealth < 50 && currentHealth > 0)
         {
             currentPhase = phases.phase3;
         }
@@ -38,17 +49,37 @@ public class CyclopsPhases : MonoBehaviour
 
     void actOnPhases(phases phase)
     {
+        int randomIndex;
+        string randomTrigger;
+
         if (phase == phases.phase1)
         {
-            //insert code for behaviour
+            randomIndex = Random.Range(0, phaseOneTriggerNames.Length);
+            randomTrigger = phaseOneTriggerNames[randomIndex];
+            bossAnimator.SetTrigger(randomTrigger);
         }
-        else if(phase == phases.phase2)
+        else if (phase == phases.phase2)
         {
-            //insert code for behaviour
+            randomIndex = Random.Range(0, phaseTwoTriggerNames.Length);
+            randomTrigger = phaseTwoTriggerNames[randomIndex];
+            bossAnimator.SetTrigger(randomTrigger);
         }
         else if (phase == phases.phase3)
         {
-            //insert code for behaviour
+            randomIndex = Random.Range(0, phaseThreeTriggerNames.Length);
+            randomTrigger = phaseThreeTriggerNames[randomIndex];
+            bossAnimator.SetTrigger(randomTrigger);
         }
     }
+
+
+
+    IEnumerator initiateActionCooldown()
+    {
+        canAction = false;
+        yield return new WaitForSeconds(actionCooldownTime);
+        canAction = true;
+    }
 }
+
+
